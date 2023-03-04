c1f7401f3c01c2a453f380d184e981f9b7be8ae7
Mohammad Bahmani <mohammadmandegar2019@gmail.com>
2023-03-04 17:51:10 +0430

    changes in words

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ let score = 0;
 let time = 0;
 
 function getRandomWord() {
-  return words[Math.floor(Math.random * words.length)];
+  return words[Math.floor(Math.random() * words.length)];
 }
 console.log(words.length);
+console.log(getRandomWord());
