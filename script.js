7da69a0d23dc254737acd32a9ad0c1f7f0443acb
Mohammad Bahmani <mohammadmandegar2019@gmail.com>
2023-03-04 17:47:41 +0430

    initializing words, score

--- script.js
+++ script.js
@@ -7,3 +7,36 @@ const settingBtn = document.querySelector("settings-btn");
 const settings = document.querySelector("settings");
 const settingsForm = document.querySelector("settings-form");
 const difficultySelect = document.querySelector("difficulty");
+
+const words = [
+  "feeble",
+  "admit",
+  "apple",
+  "computer",
+  "tissue",
+  "book",
+  "comprehnesive",
+  "education",
+  "knowledge",
+  "man",
+  "afghanistan",
+  "technology",
+  "mouse",
+  "screen",
+  "lamp",
+  "mobile",
+  "javascript",
+  "card",
+];
+
+//init randomWord
+let randomWord;
+//init score
+let score = 0;
+//init time
+let time = 0;
+
+function getRandomWord() {
+  return words[Math.floor(Math.random * words.length)];
+}
+console.log(words.length);
